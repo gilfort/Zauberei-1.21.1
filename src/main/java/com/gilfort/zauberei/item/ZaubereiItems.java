8b1ba031f8a1edbec3a3fd8e4f66739c6b356e57
gilfort <6735454+gilfort@users.noreply.github.com>
2025-01-13 20:41:15 +0100

    fully implemented set effect mechanic

--- src/main/java/com/gilfort/zauberei/item/ZaubereiItems.java
+++ src/main/java/com/gilfort/zauberei/item/ZaubereiItems.java
@@ -3,8 +3,10 @@ package com.gilfort.zauberei.item;
 import com.gilfort.zauberei.Zauberei;
 import com.gilfort.zauberei.item.custom.WandItem;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.Item;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
@@ -21,6 +23,16 @@ public class ZaubereiItems {
     //setting up special itemclasses
     public static final DeferredItem<Item> WAND = ITEMS.register("wand",
             () -> new WandItem(new Item.Properties()));
+
+    //setting up armor items
+    public static final DeferredHolder<Item, Item> MAGICCLOTH_HELMET = ITEMS.register("magiccloth_helmet",
+            () -> new ZaubereiMagicclothArmorItem(ArmorItem.Type.HELMET));
+    public static final DeferredHolder<Item, Item> MAGICCLOTH_CHESTPLATE = ITEMS.register("magiccloth_chestplate",
+            () -> new ZaubereiMagicclothArmorItem(ArmorItem.Type.CHESTPLATE));
+    public static final DeferredHolder<Item, Item> MAGICCLOTH_LEGGINGS = ITEMS.register("magiccloth_leggings",
+            () -> new ZaubereiMagicclothArmorItem(ArmorItem.Type.LEGGINGS));
+    public static final DeferredHolder<Item, Item> MAGICCLOTH_BOOTS = ITEMS.register("magiccloth_boots",
+            () -> new ZaubereiMagicclothArmorItem(ArmorItem.Type.BOOTS));
 
 
     public static void register(IEventBus eventBus) {

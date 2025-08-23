384907cd192ee6525d4150ce6b099d880d780f60
gilfort <6735454+gilfort@users.noreply.github.com>
2025-08-23 14:24:06 +0200

    Add enchantment tags for Magiccloth armor

--- src/main/java/com/gilfort/zauberei/datagen/ZaubereiItemTagProvider.java
+++ src/main/java/com/gilfort/zauberei/datagen/ZaubereiItemTagProvider.java
@@ -1,11 +1,13 @@
 package com.gilfort.zauberei.datagen;
 
 import com.gilfort.zauberei.Zauberei;
+import com.gilfort.zauberei.item.ZaubereiItems;
 import com.gilfort.zauberei.util.ZaubereiTags;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.ItemTagsProvider;
-import net.minecraft.world.item.Items;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.world.level.block.Block;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.Nullable;
@@ -20,13 +22,43 @@ public class ZaubereiItemTagProvider extends ItemTagsProvider {
 
     @Override
     protected void addTags(HolderLookup.Provider provider) {
-        tag(ZaubereiTags.Items.TRANSFORMABLE_ITEMS)
-//                .add(ModItems.BISMUTH.get())
-//                .add(ModItems.RAW_BISMUTH.get())
-//                .add(Items.COAL)
-//                .add(Items.STICK)
-//                .add(Items.COMPASS);
-        ;
+        tag(ItemTags.create(ResourceLocation.fromNamespaceAndPath("minecraft", "enchantable/armor")))
+                .add(
+                        ZaubereiItems.MAGICCLOTH_HELMET.get(),
+                        ZaubereiItems.MAGICCLOTH_HELMET_ALT.get(),
+                        ZaubereiItems.MAGICCLOTH_CHESTPLATE.get(),
+                        ZaubereiItems.MAGICCLOTH_CHESTPLATE_ALT.get(),
+                        ZaubereiItems.MAGICCLOTH_LEGGINGS.get(),
+                        ZaubereiItems.MAGICCLOTH_LEGGINGS_ALT.get(),
+                        ZaubereiItems.MAGICCLOTH_BOOTS.get(),
+                        ZaubereiItems.MAGICCLOTH_BOOTS_ALT.get()
+                );
+
+        tag(ItemTags.create(ResourceLocation.fromNamespaceAndPath("minecraft", "enchantable/helmet")))
+                .add(
+                        ZaubereiItems.MAGICCLOTH_HELMET.get(),
+                        ZaubereiItems.MAGICCLOTH_HELMET_ALT.get()
+                );
+
+        tag(ItemTags.create(ResourceLocation.fromNamespaceAndPath("minecraft", "enchantable/chestplate")))
+                .add(
+                        ZaubereiItems.MAGICCLOTH_CHESTPLATE.get(),
+                        ZaubereiItems.MAGICCLOTH_CHESTPLATE_ALT.get()
+                );
+
+        tag(ItemTags.create(ResourceLocation.fromNamespaceAndPath("minecraft", "enchantable/leggings")))
+                .add(
+                        ZaubereiItems.MAGICCLOTH_LEGGINGS.get(),
+                        ZaubereiItems.MAGICCLOTH_LEGGINGS_ALT.get()
+                );
+
+        tag(ItemTags.create(ResourceLocation.fromNamespaceAndPath("minecraft", "enchantable/boots")))
+                .add(
+                        ZaubereiItems.MAGICCLOTH_BOOTS.get(),
+                        ZaubereiItems.MAGICCLOTH_BOOTS_ALT.get()
+                );
+
+        tag(ZaubereiTags.Items.TRANSFORMABLE_ITEMS);
 
     }
 }
